5ced75f1006eda84a4ba90499da82b96cc051421
kkf1 <2644863636@qq.com>
2023-03-09 19:34:15 +0800

    [feat] improve list interface's performance

---
+++ server/cache/kv_cache.go
@@ -0,0 +1,153 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/apache/servicecomb-kie/pkg/model"
+	"github.com/apache/servicecomb-kie/pkg/stringutil"
+	"github.com/go-chassis/foundation/backoff"
+	"github.com/go-chassis/openlog"
+	"github.com/little-cui/etcdadpt"
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	"strings"
+	"time"
+)
+
+func Init() {
+	Kc = NewKvCache(PrefixKvs, 0, labelsToIDsMap{})
+	go Kc.refresh(context.Background())
+}
+
+var Kc *KvCache
+
+const (
+	PrefixKvs = "kvs"
+)
+
+type labelsToIDsMap map[string]map[string]struct{}
+
+type KvCache struct {
+	Client   etcdadpt.Client
+	Prefix   string
+	Revision int64
+	Cache    labelsToIDsMap
+}
+
+func NewKvCache(prefix string, rev int64, cache labelsToIDsMap) *KvCache {
+	return &KvCache{
+		Client:   etcdadpt.Instance(),
+		Prefix:   prefix,
+		Revision: rev,
+		Cache:    cache,
+	}
+}
+
+func (kc *KvCache) refresh(ctx context.Context) {
+	openlog.Info("start to list and watch")
+	retries := 0
+
+	timer := time.NewTimer(time.Second)
+	defer timer.Stop()
+	for {
+		nextPeriod := time.Second
+		if err := kc.listWatch(ctx); err != nil {
+			retries++
+			nextPeriod = backoff.GetBackoff().Delay(retries)
+		} else {
+			retries = 0
+		}
+
+		select {
+		case <-ctx.Done():
+			openlog.Info("stop to list and watch")
+			return
+		case <-timer.C:
+			timer.Reset(nextPeriod)
+		}
+	}
+}
+
+func (kc *KvCache) listWatch(ctx context.Context) error {
+	rsp, err := kc.Client.Do(ctx, etcdadpt.WatchPrefixOpOptions(kc.Prefix)...)
+	if err != nil {
+		openlog.Error(fmt.Sprintf("list prefix %s failed, current rev: %d, err, %v", kc.Prefix, kc.Revision, err))
+		return err
+	}
+	kc.Revision = rsp.Revision
+
+	kc.cachePut(rsp)
+
+	rev := kc.Revision
+	opts := append(
+		etcdadpt.WatchPrefixOpOptions(kc.Prefix),
+		etcdadpt.WithRev(kc.Revision+1),
+		etcdadpt.WithWatchCallback(kc.watchCallBack),
+	)
+	err = kc.Client.Watch(ctx, opts...)
+	if err != nil {
+		openlog.Error(fmt.Sprintf("watch prefix %s failed, start rev: %d+1->%d->0, err %v", kc.Prefix, rev, kc.Revision, err))
+		kc.Revision = 0
+	}
+	return err
+}
+
+func (kc *KvCache) watchCallBack(message string, rsp *etcdadpt.Response) error {
+	if rsp == nil || len(rsp.Kvs) == 0 {
+		return fmt.Errorf("unknown event")
+	}
+	kc.Revision = rsp.Revision
+
+	switch rsp.Action {
+	case etcdadpt.ActionPut:
+		kc.cachePut(rsp)
+	case etcdadpt.ActionDelete:
+		kc.cacheDelete(rsp)
+	default:
+		openlog.Warn(fmt.Sprintf("unrecognized action::%v", rsp.Action))
+	}
+	return nil
+}
+
+func (kc *KvCache) cachePut(rsp *etcdadpt.Response) {
+	for _, kv := range rsp.Kvs {
+		kvDoc, inputKey, err := getInputKey(kv)
+		if err != nil {
+			continue
+		}
+		if _, ok := kc.Cache[inputKey]; !ok {
+			kc.Cache[inputKey] = map[string]struct{}{}
+		}
+		kc.Cache[inputKey][kvDoc.ID] = struct{}{}
+	}
+}
+
+func (kc *KvCache) cacheDelete(rsp *etcdadpt.Response) {
+	for _, kv := range rsp.Kvs {
+		kvDoc, inputKey, err := getInputKey(kv)
+		if err != nil {
+			continue
+		}
+		if _, ok := kc.Cache[inputKey]; !ok {
+			continue
+		}
+		delete(kc.Cache[inputKey], kvDoc.ID)
+	}
+}
+
+func getInputKey(kv *mvccpb.KeyValue) (*model.KVDoc, string, error) {
+	kvDoc := &model.KVDoc{}
+	err := json.Unmarshal(kv.Value, kvDoc)
+	if err != nil {
+		openlog.Error(fmt.Sprintf("failed to unmarshal kv, err %v", err))
+		return nil, "", err
+	}
+	labelFormat := stringutil.FormatMap(kvDoc.Labels)
+	inputKey := strings.Join([]string{
+		"",
+		kvDoc.Domain,
+		kvDoc.Project,
+		labelFormat,
+	}, "/")
+	return kvDoc, inputKey, nil
+}
--- server/datasource/etcd/kv/kv_dao.go
+++ server/datasource/etcd/kv/kv_dao.go
@@ -20,10 +20,13 @@ package kv
 import (
 	"context"
 	"encoding/json"
+	"github.com/apache/servicecomb-kie/pkg/stringutil"
+	"github.com/apache/servicecomb-kie/server/cache"
 	"regexp"
 	"strings"
+	"sync"
 
-	"github.com/go-chassis/cari/sync"
+	kieSync "github.com/go-chassis/cari/sync"
 	"github.com/go-chassis/openlog"
 	"github.com/little-cui/etcdadpt"
 
@@ -84,7 +87,7 @@ func txnCreate(ctx context.Context, kv *model.KVDoc) (bool, error) {
 		openlog.Error("fail to marshal kv " + err.Error())
 		return false, err
 	}
-	task, err := sync.NewTask(kv.Domain, kv.Project, sync.CreateAction, datasource.ConfigResource, kv)
+	task, err := kieSync.NewTask(kv.Domain, kv.Project, kieSync.CreateAction, datasource.ConfigResource, kv)
 	if err != nil {
 		openlog.Error("fail to create task" + err.Error())
 		return false, err
@@ -154,7 +157,7 @@ func txnUpdate(ctx context.Context, kv *model.KVDoc) error {
 		openlog.Error(err.Error())
 		return err
 	}
-	task, err := sync.NewTask(kv.Domain, kv.Project, sync.UpdateAction, datasource.ConfigResource, kv)
+	task, err := kieSync.NewTask(kv.Domain, kv.Project, kieSync.UpdateAction, datasource.ConfigResource, kv)
 	if err != nil {
 		openlog.Error("fail to create task" + err.Error())
 		return err
@@ -278,7 +281,7 @@ func txnFindOneAndDelete(ctx context.Context, kvID, project, domain string) (*mo
 		openlog.Error(err.Error())
 		return nil, err
 	}
-	task, err := sync.NewTask(domain, project, sync.DeleteAction, datasource.ConfigResource, kvDoc)
+	task, err := kieSync.NewTask(domain, project, kieSync.DeleteAction, datasource.ConfigResource, kvDoc)
 	if err != nil {
 		openlog.Error("fail to create task" + err.Error())
 		return nil, err
@@ -288,7 +291,7 @@ func txnFindOneAndDelete(ctx context.Context, kvID, project, domain string) (*mo
 		openlog.Error("fail to marshal task" + err.Error())
 		return nil, err
 	}
-	tombstone := sync.NewTombstone(domain, project, datasource.ConfigResource, datasource.TombstoneID(kvDoc))
+	tombstone := kieSync.NewTombstone(domain, project, datasource.ConfigResource, datasource.TombstoneID(kvDoc))
 	tombstoneBytes, err := json.Marshal(tombstone)
 	if err != nil {
 		openlog.Error("fail to marshal tombstone" + err.Error())
@@ -383,8 +386,8 @@ func txnFindManyAndDelete(ctx context.Context, kvIDs []string, project, domain s
 	var opOptions []etcdadpt.OpOptions
 	kvTotalNum := len(kvIDs)
 	docs = make([]*model.KVDoc, kvTotalNum)
-	tasks := make([]*sync.Task, kvTotalNum)
-	tombstones := make([]*sync.Tombstone, kvTotalNum)
+	tasks := make([]*kieSync.Task, kvTotalNum)
+	tombstones := make([]*kieSync.Tombstone, kvTotalNum)
 	successKVNum := 0
 	for i := 0; i < kvTotalNum; i++ {
 		kvDoc, err := getKVDoc(ctx, domain, project, kvIDs[i])
@@ -399,14 +402,14 @@ func txnFindManyAndDelete(ctx context.Context, kvIDs []string, project, domain s
 		if kvDoc == nil {
 			continue
 		}
-		task, err := sync.NewTask(domain, project, sync.DeleteAction, datasource.ConfigResource, kvDoc)
+		task, err := kieSync.NewTask(domain, project, kieSync.DeleteAction, datasource.ConfigResource, kvDoc)
 		if err != nil {
 			openlog.Error("fail to create task")
 			return nil, 0, err
 		}
 		docs[successKVNum] = kvDoc
 		tasks[successKVNum] = task
-		tombstones[successKVNum] = sync.NewTombstone(domain, project, datasource.ConfigResource,
+		tombstones[successKVNum] = kieSync.NewTombstone(domain, project, datasource.ConfigResource,
 			datasource.TombstoneID(kvDoc))
 		successKVNum++
 	}
@@ -522,11 +525,83 @@ func (s *Dao) listData(ctx context.Context, project, domain string, options ...d
 	if err != nil {
 		return nil, opts, err
 	}
-	// TODO may be OOM
-	kvs, _, err := etcdadpt.List(ctx, key.KVList(domain, project))
+
+	labelFormat := stringutil.FormatMap(opts.Labels)
+	inputKey := strings.Join([]string{
+		"",
+		domain,
+		project,
+		labelFormat,
+	}, "/")
+	if cache.Kc != nil {
+		kvIDs, ok := cache.Kc.Cache[inputKey]
+		if ok && opts.ExactLabels {
+			result, err := cacheSearch(ctx, project, domain, kvIDs, opts, regex)
+			if err != nil {
+				openlog.Error("list kv failed: " + err.Error())
+				return nil, opts, nil
+			}
+			return result, opts, nil
+		}
+
+		if !ok {
+			cache.Kc.Cache[inputKey] = map[string]struct{}{}
+		}
+	}
+
+	result, err := matchLabelsSearch(ctx, domain, project, regex, opts)
 	if err != nil {
 		openlog.Error("list kv failed: " + err.Error())
 		return nil, opts, err
+	}
+
+	return result, opts, nil
+}
+
+func cacheSearch(ctx context.Context, project string, domain string, kvIDs map[string]struct{}, opts datasource.FindOptions, regex *regexp.Regexp) (*model.KVResponse, error) {
+	openlog.Debug("using cache to search kv")
+	result := &model.KVResponse{}
+	wg := sync.WaitGroup{}
+	for kvID := range kvIDs {
+		wg.Add(1)
+		go func(kvID string) {
+			defer wg.Done()
+			kv, err := etcdadpt.Get(ctx, key.KV(domain, project, kvID))
+			if err != nil {
+				openlog.Error("list kv failed: " + err.Error())
+				return
+			}
+			var doc model.KVDoc
+			err = json.Unmarshal(kv.Value, &doc)
+			if err != nil {
+				openlog.Error("decode to KVList error: " + err.Error())
+				return
+			}
+
+			if opts.Status != "" && doc.Status != opts.Status {
+				return
+			}
+			if regex != nil && !regex.MatchString(doc.Key) {
+				return
+			}
+
+			datasource.ClearPart(&doc)
+			result.Data = append(result.Data, &doc)
+			result.Total++
+			if IsUniqueFind(opts) {
+				return
+			}
+		}(kvID)
+	}
+	wg.Wait()
+	return result, nil
+}
+
+func matchLabelsSearch(ctx context.Context, domain, project string, regex *regexp.Regexp, opts datasource.FindOptions) (*model.KVResponse, error) {
+	openlog.Debug("using labels to search kv")
+	kvs, _, err := etcdadpt.List(ctx, key.KVList(domain, project))
+	if err != nil {
+		return nil, err
 	}
 	result := &model.KVResponse{
 		Data: []*model.KVDoc{},
@@ -551,7 +626,7 @@ func (s *Dao) listData(ctx context.Context, project, domain string, options ...d
 		}
 	}
 
-	return result, opts, nil
+	return result, nil
 }
 
 func IsUniqueFind(opts datasource.FindOptions) bool {
